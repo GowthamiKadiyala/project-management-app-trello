adc1321d8aca58c287129a058c5298f58e2eb53f
Gowthami Kadiyala <gowthamikadiyala@Gowthamis-MacBook-Air.local>
2025-12-27 14:13:52 -0500

    Fix CORS and add frontend URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,10 +10,15 @@ models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",  
+    "http://localhost:3000", 
+    "https://project-management-app-trello-frontend.onrender.com" 
+]
 # Allow React to talk to this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=[origins], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
